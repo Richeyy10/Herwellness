9374be8656eb641461ce9ebacde2d1e14136d395
righteousness <righteousness@righteousness-MacBook-Air.local>
2025-12-08 15:19:24 +0100

    Route corrected

--- src/app/api/blog/create-post/route.ts
+++ src/app/api/blog/create-post/route.ts
@@ -1,27 +1,25 @@
-import connectDB from '../../../../config/db';
-import BlogPost, { IBlogPost } from '../../../../models/blogpost';
-import { NextRequest, NextResponse } from 'next/server';
+import connectDB from "../../../../config/db";
+import BlogPost, { IBlogPost } from "../../../../models/blogpost";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(req: NextRequest) {
-  if (req.method !== 'POST') {
-    return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
-  }
-
+export async function POST(req: NextRequest) {
   try {
     await connectDB();
+
     const postData: Partial<IBlogPost> = await req.json();
     const newPost = await BlogPost.create(postData);
 
     return NextResponse.json(
       {
         success: true,
-        message: 'Blog post created successfully',
+        message: "Blog post created successfully",
         data: newPost,
       },
       { status: 201 }
     );
   } catch (error) {
     console.error(error);
+
     return NextResponse.json(
       { success: false, message: (error as Error).message },
       { status: 400 }
